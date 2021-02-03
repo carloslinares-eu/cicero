eae68729c79db2e21d8d1301407e1164b41f7f84
Harsh Sinha <harshks534@gmail.com>
2021-02-03 21:20:15 +0530

    modified setup.py converting ppt text to speech in English language

--- setup.py
+++ setup.py
@@ -1,22 +1,12 @@
-import setuptools
+from gtts import gTTS
+import os
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+myText = "Text To Speech Conversion Using Python"
 
-setuptools.setup(
-    name="cicero",
-    version="0.1",
-    author="Carlos Linares",
-    author_email="contact@carloslinares.eu",
-    description="Powerpoint Translation Tool",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/carloslinares-eu/cicero",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU GPLv3",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.8',
-)
+language = 'en'
+
+output = gTTS(Text=myText, lang=language, slow=False)
+
+output.save("output.mp3")
+
+os.system("start output.mp3")
