674d782b26531a652447c50b5aea4a3c41d02484
Carlos Linares <contact@carloslinares.eu>
2021-02-03 14:16:21 +0100

    Adapting project to Linux

--- cicero/powerloader.py
+++ cicero/powerloader.py
@@ -3,13 +3,20 @@ import pptx
 
 def extract_notes(pptx_file_path):
     active_pptx = pptx.Presentation(pptx_file_path)
-    data = {'slides': []}
+    data = {'speaker_notes': []}
 
     for slide_number, slide in enumerate(active_pptx.slides):
         text_note = slide.notes_slide.notes_text_frame.text
-        data['slides'].append({
+        data['speaker_notes'].append({
             'slide_number': slide_number,
-            'speaker_notes': text_note
+            'text': text_note
         })
     return data
 
+def extract_text(pptx_file_path):
+    active_pptx = pptx.Presentation(pptx_file_path)
+    speaker_notes = {'slides': []}
+
+
+notes = extract_notes("/home/carlos/Documentos/cicero/200411 MBA AEROSP Sevilla Exercise 5 Channels (in class).pptx")
+print(notes)
