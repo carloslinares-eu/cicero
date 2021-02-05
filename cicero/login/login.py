5909b32a717b24a0049c6b80b754ba96d99a7572
Carlos Linares <contact@carloslinares.eu>
2021-02-05 15:44:53 +0100

    Finished login module

--- cicero/login/login.py
+++ cicero/login/login.py
@@ -1,19 +1,72 @@
-from google.oauth2 import service_account
+import tkinter.ttk
+import tkinter.filedialog
+import tkinter.messagebox
+import google.oauth2.service_account
+import google.cloud.translate_v2
+import google.auth.exceptions
 
 
-def login_with_json(credentials_file_path):
+class LoginApp:
+    def __init__(self, main_app):
+        self.main_app = main_app
+        self.root = tkinter.Tk()
+        self.root.title("CICERO - Login")
+        self.root.geometry("400x120")
+        self.root.resizable(0, 0)
+        self.root.iconbitmap("cicero/resources/cicero.ico")
+        self.text_format_01 = ("Segoe UI", 10)
 
-    try:
-        credentials = service_account.Credentials.from_service_account_file(credentials_file_path)
-    except OSError:
-        print('File not found')
+        self.license_label = tkinter.Label(self.root, text="Select License File", anchor="w", font=self.text_format_01)
+        self.license_entry = tkinter.Entry(self.root, font=self.text_format_01, width=38)
+        self.browse_button = tkinter.ttk.Button(self.root, text="Browse", command=self.browse_service_account_file)
+        self.login_button = tkinter.ttk.Button(self.root, text="Login", command=self.login_into_main_app)
 
-    scoped_credentials = credentials.with_scopes(['https://www.googleapis.com/auth/cloud-platform'])
+        self.credentials = None
+        self.credentials_scoped = None
+        self.is_logged_in = False
+        self.valid_license_filetypes = (("JavaScript Object Notation", "*.json"), ("All files", "*.*"))
 
-    return credentials, scoped_credentials
+    def initialize(self):
+        self.license_label.grid(padx=20, pady=10, row=0, column=0, columnspan=2, sticky="W")
+        self.license_entry.grid(padx=20, pady=0, row=1, column=0, columnspan=2)
+        self.browse_button.grid(row=1, column=2, sticky="W")
+        self.login_button.grid(row=2, column=2, pady=8, sticky="W")
+        self.root.mainloop()
 
+    def browse_service_account_file(self):
+        prompt_title = "Open License File"
+        license_file = tkinter.filedialog.askopenfile(filetypes=self.valid_license_filetypes, title=prompt_title)
+        self.license_entry.delete(0, 'end')
+        self.license_entry.insert(0, license_file.name)
 
-credentials, scoped_credentials = login_with_json
+    def load_service_account_file(self):
+        file = self.license_entry.get()
+        scope = ['https://www.googleapis.com/auth/cloud-platform']
+        try:
+            self.credentials = google.oauth2.service_account.Credentials.from_service_account_file(file)
+            self.credentials_scoped = self.credentials.with_scopes(scope)
 
+        except FileNotFoundError:
+            tkinter.messagebox.showerror(title="Critical error", message="File not found")
 
-print("Finished")
+        except AttributeError:
+            tkinter.messagebox.showerror(title="Critical error", message="Incorrect credentials file")
+
+    def check_service_account_file(self):
+        translate_client = google.cloud.translate_v2.Client(credentials=self.credentials)
+        try:
+            translate_client.translate("TEST", target_language="EN")
+            self.is_logged_in = True
+
+        except google.auth.exceptions.RefreshError:
+            refresh_error_message = "The credentials' access token failed.\n"
+            refresh_error_message += "If the problem persist the key has probably expired."
+            tkinter.messagebox.showerror(title="Critical error", message=refresh_error_message)
+            self.is_logged_in = False
+
+    def login_into_main_app(self):
+        self.load_service_account_file()
+        self.check_service_account_file()
+        if self.is_logged_in is True:
+            self.root.destroy()
+            self.main_app.root.mainloop()
