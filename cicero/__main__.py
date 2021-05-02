e25442dc50ffcebb3b8b8fd2f37af5b5fcc0ab7d
carloslinares-eu <contact@carloslinares.eu>
2021-05-02 21:24:33 +0200

    Fixing relative import in __main__.py

--- cicero/__main__.py
+++ cicero/__main__.py
@@ -1,15 +1,13 @@
 import login
 import pptxtranslator
-from login import LoginApp
-from pptxtranslator import TranslationApp
 
 
 def main():
-    cicero_login_app = LoginApp()
+    cicero_login_app = login.LoginApp()
     cicero_login_app.initialize()
 
     if cicero_login_app.has_logged_in is True:
-        main_translation_app = TranslationApp(credentials=cicero_login_app.credentials)
+        main_translation_app = pptxtranslator.TranslationApp(credentials=cicero_login_app.credentials)
         main_translation_app.initialize()
 
 
